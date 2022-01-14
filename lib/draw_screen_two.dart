f3d2205316b574912cc85936ee146216656bd6b2
AMJAD-ALI <amjadjamali06@gmail.com>
2022-01-14 10:44:59 +0500

    Initial Commit For Drawing View. 1. Added new drawing screen and animated fab buttons.

---
+++ lib/draw_screen_two.dart
@@ -0,0 +1,370 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_drawing_screen/animated_widget/animated_fab.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+//void main() => runApp(CanvasPainting());
+
+class CanvasPainting extends StatefulWidget {
+  const CanvasPainting({Key? key}) : super(key: key);
+
+  @override
+  _CanvasPaintingState createState() => _CanvasPaintingState();
+}
+
+class _CanvasPaintingState extends State<CanvasPainting> {
+  GlobalKey globalKey = GlobalKey();
+
+  List<TouchPoints?> points = [];
+  double opacity = 1.0;
+  StrokeCap strokeType = StrokeCap.round;
+  double strokeWidth = 3.0;
+  Color selectedColor = Colors.black;
+
+  Future<void> _pickStroke() async {
+    //Shows AlertDialog
+    return showDialog<void>(
+      context: context,
+
+      //Dismiss alert dialog when set true
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        //Clips its child in a oval shape
+        return ClipOval(
+          child: AlertDialog(
+            //Creates three buttons to pick stroke value.
+            actions: <Widget>[
+              //Resetting to default stroke value
+              FlatButton(
+                child: Icon(
+                  Icons.clear,
+                ),
+                onPressed: () {
+                  strokeWidth = 3.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 24,
+                ),
+                onPressed: () {
+                  strokeWidth = 10.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 40,
+                ),
+                onPressed: () {
+                  strokeWidth = 30.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 60,
+                ),
+                onPressed: () {
+                  strokeWidth = 50.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _opacity() async {
+    //Shows AlertDialog
+    return showDialog<void>(
+      context: context,
+
+      //Dismiss alert dialog when set true
+      barrierDismissible: true,
+
+      builder: (BuildContext context) {
+        //Clips its child in a oval shape
+        return ClipOval(
+          child: AlertDialog(
+            //Creates three buttons to pick opacity value.
+            actions: <Widget>[
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 24,
+                ),
+                onPressed: () {
+                  //most transparent
+                  opacity = 0.1;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 40,
+                ),
+                onPressed: () {
+                  opacity = 0.5;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 60,
+                ),
+                onPressed: () {
+                  //not transparent at all.
+                  opacity = 1.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String> _save(BuildContext context) async {
+    RenderRepaintBoundary? boundary = context.findAncestorRenderObjectOfType<RenderRepaintBoundary>();
+    if(boundary==null){
+      return "Render Repaint Boundary not Found!";
+    }
+    ui.Image image = await boundary.toImage();
+    ByteData byteData = (await image.toByteData(format: ui.ImageByteFormat.png))??ByteData(0);
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+
+    //Request permissions if not already granted
+    // if (!(await Permission.storage.status.isGranted))
+    //   await Permission.storage.request();
+
+    final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(pngBytes),
+        quality: 100,
+        name: "canvas_image");
+    return (result!=null && result["isSuccess"]) ? "Success"
+        : (result!=null) ? result["errorMessage"]??"":"";
+  }
+
+  List<Widget> fabOption() {
+    return <Widget>[
+      FloatingActionButton(
+        heroTag: "paint_save",
+        child: Icon(Icons.save),
+        tooltip: 'Save',
+        onPressed: () {
+          //min: 0, max: 50
+          setState(() {
+            _save(context);
+          });
+        },
+      ),
+      FloatingActionButton(
+        heroTag: "paint_stroke",
+        child: Icon(Icons.brush),
+        tooltip: 'Stroke',
+        onPressed: () {
+          //min: 0, max: 50
+          setState(() {
+            _pickStroke();
+          });
+        },
+      ),
+      FloatingActionButton(
+        heroTag: "paint_opacity",
+        child: Icon(Icons.opacity),
+        tooltip: 'Opacity',
+        onPressed: () {
+          //min:0, max:1
+          setState(() {
+            _opacity();
+          });
+        },
+      ),
+      FloatingActionButton(
+          heroTag: "erase",
+          child: Icon(Icons.clear),
+          tooltip: "Erase",
+          onPressed: () {
+            setState(() {
+              points.clear();
+            });
+          }),
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_red",
+        child: colorMenuItem(Colors.red),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.red;
+          });
+        },
+      ),
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_green",
+        child: colorMenuItem(Colors.green),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.green;
+          });
+        },
+      ),
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_pink",
+        child: colorMenuItem(Colors.pink),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.pink;
+          });
+        },
+      ),
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_blue",
+        child: colorMenuItem(Colors.blue),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.blue;
+          });
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GestureDetector(
+          onPanUpdate: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject() as RenderBox;
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanStart: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject() as RenderBox;
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanEnd: (details) {
+            setState(() {
+              points.add(null);
+            });
+          },
+          child: RepaintBoundary(
+            key: globalKey,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset("assets/images/hut.png"),
+                ),
+                CustomPaint(
+                  size: Size.infinite,
+                  painter: MyPainter(
+                    pointsList: points,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: AnimatedFloatingActionButton(
+          fabButtons: fabOption(),
+          colorStartAnimation: Colors.blue,
+          colorEndAnimation: Colors.cyan,
+          animatedIconData: AnimatedIcons.menu_close,
+        ),
+      ),
+    );
+  }
+
+  Widget colorMenuItem(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+      child: ClipOval(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          height: 36,
+          width: 36,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  MyPainter({required this.pointsList});
+
+  //Keep track of the points tapped on the screen
+  List<TouchPoints?> pointsList;
+  List<Offset> offsetPoints = [];
+
+  //This is where we can draw on canvas.
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i + 1] != null) {
+        //Drawing line when two consecutive points are available
+        canvas.drawLine(pointsList[i]!.points, pointsList[i + 1]!.points, pointsList[i]!.paint);
+      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+        offsetPoints.clear();
+        offsetPoints.add(pointsList[i]!.points);
+        offsetPoints.add(Offset(pointsList[i]!.points.dx + 0.1, pointsList[i]!.points.dy + 0.1));
+
+        //Draw points when two points are not next to each other
+        canvas.drawPoints(ui.PointMode.points, offsetPoints, pointsList[i]!.paint);
+      }
+    }
+  }
+
+  //Called when CustomPainter is rebuilt.
+  //Returning true because we want canvas to be rebuilt to reflect new changes.
+  @override
+  bool shouldRepaint(MyPainter oldDelegate) => true;
+}
+
+//Class to define a point touched at canvas
+class TouchPoints {
+  Paint paint;
+  Offset points;
+  TouchPoints({required this.points, required this.paint});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_drawing_screen/draw_screen_two.dart';
 
 import 'draw_screen.dart';
 
@@ -30,6 +31,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Drawing Demo"),
+        actions: [IconButton(icon: const Icon(Icons.edit), onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => const CanvasPainting()),))],
       ),
       body: const Center(
         child: Text(
